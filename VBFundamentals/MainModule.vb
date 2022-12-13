0f51576793f8edf51e6b5479d259d44e5d77cb13
Casey Spaulding <casey.spaulding@me.com>
2022-12-13 13:41:47 -0500

    updated notes

--- VBFundamentals/MainModule.vb
+++ VBFundamentals/MainModule.vb
@@ -1,11 +1,29 @@
-﻿Module MainModule
+﻿Imports System.Runtime.Remoting.Channels
+
+Module MainModule
+    Dim Name As String = "Mountain Bike"
 
     Sub Main()
-        Dim ProductID As Integer
-        Dim isActive As Boolean
-        Dim Name As String
-        Dim ListPrice As Decimal
-        Dim SellStartDate As DateTime
+        Console.WriteLine(Name)
+
+
+        Console.ReadKey()
+
+
+
+        'varibles are declared with the Dim keyword
+        'Dim ProductID As Integer
+        'Dim isActive As Boolean
+        'Dim Name As String
+        'Dim ListPrice As Decimal
+        'Dim SellStartDate As DateTime
+
+        'ProductID = 1
+        'isActive = True
+        'Name = "10 Speed Bike"
+        'ListPrice = 999.99D
+        'SellStartDate = #1/1/2019#
+        'The "#" Is the Type Literal for a DateTime value.
 
 
     End Sub
