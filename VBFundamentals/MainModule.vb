781a05dfdb9a01a7c1e56ac457f0635b3dbe22e6
Casey Spaulding <casey.spaulding@me.com>
2022-12-13 12:15:53 -0500

    Add project files.

---
+++ VBFundamentals/MainModule.vb
@@ -0,0 +1,13 @@
+﻿Module MainModule
+
+    Sub Main()
+        Dim ProductID As Integer
+        Dim isActive As Boolean
+        Dim Name As String
+        Dim ListPrice As Decimal
+        Dim SellStartDate As DateTime
+
+
+    End Sub
+
+End Module
