23c797fe20cec2cd7c23078bee22f92953bbe09b
Casey Spaulding <casey.spaulding@me.com>
2022-12-14 06:57:50 -0500

    added class

--- VBFundamentals/MainModule.vb
+++ VBFundamentals/MainModule.vb
@@ -4,7 +4,15 @@ Module MainModule
     Dim Name As String = "Mountain Bike"
 
     Sub Main()
+        If True Then
+            Dim Name As String = "Tricycle"
+            Dim ListPrice As Decimal = 59.99
+        End If
+
+
         Console.WriteLine(Name)
+        'The following does not compile
+        'Console.WriteLine(ListPrice)
 
 
         Console.ReadKey()
---
+++ VBFundamentals/Product.vb
@@ -0,0 +1,12 @@
+﻿Public Class Product
+    Private _IsActive As Boolean
+    Public Property IsActive() As Boolean
+        Get
+            Return _IsActive
+        End Get
+        Set(ByVal value As Boolean)
+            _IsActive = value
+        End Set
+    End Property
+
+End Class
